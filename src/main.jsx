c43e4cbf68d871aa444b6bf206b3d204304f7e7e
andykay-dev <andrewkolo@gmail.com>
2026-05-26 21:16:54 +1000

    Add files via upload
    complete rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./storage-shim.js";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/react'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+Sentry.init({
+  dsn: "https://c9758b6fa3bcda8ae679d993abf786d4@o4511436474482688.ingest.us.sentry.io/4511436483985408",
+  environment: import.meta.env.MODE,
+  tracesSampleRate: 0.2,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    new BrowserTracing(),
+    new Sentry.Replay({ maskAllText: false, blockAllMedia: false }),
+  ],
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
